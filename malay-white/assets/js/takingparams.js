4b95b78f188999e260099b37df0abeda70fa9fed
Santino Stimoli <santinostimoli@gmail.com>
2025-04-01 08:49:23 -0300

    White site updates

--- malay-white/assets/js/takingparams.js
+++ malay-white/assets/js/takingparams.js
@@ -1,15 +1,15 @@
 window.addEventListener('load', () => {
   const params = new URLSearchParams(window.location.search);
  
-  const defaultUtmP1 = 'aitest';
+  // const defaultUtmP1 = 'aitest';
   const defaultUtmP2 = 'aitest2';
   const defaultAdclid = 'aitest3';
   const defaultWidgetid = 'aitest4';
 
   // Get parameters from the URL or use defaults if they are missing or invalid
-  const utm_p1 = params.get('utm_p1') && params.get('utm_p1') !== 'null' && params.get('utm_p1').trim() !== '' 
-                 ? params.get('utm_p1') 
-                 : defaultUtmP1;
+  // const utm_p1 = params.get('utm_p1') && params.get('utm_p1') !== 'null' && params.get('utm_p1').trim() !== '' 
+  //                ? params.get('utm_p1') 
+  //                : defaultUtmP1;
   const utm_p2 = params.get('utm_p2') && params.get('utm_p2') !== 'null' && params.get('utm_p2').trim() !== '' 
                  ? params.get('utm_p2') 
                  : defaultUtmP2;
@@ -21,15 +21,15 @@ window.addEventListener('load', () => {
                  : defaultWidgetid;
 
   // Store 
-  localStorage.setItem('utm_p1', utm_p1);
+  // localStorage.setItem('utm_p1', utm_p1);
   localStorage.setItem('utm_p2', utm_p2);
   localStorage.setItem('adclid', adclid);
   localStorage.setItem('widget_id', widget_id);
 
   // Optionally, update the URL with the parameters if they were defaulted
-  if (!params.has('utm_p1') || params.get('utm_p1') === 'null' || params.get('utm_p1').trim() === '') {
-    addParamToUrl('utm_p1', utm_p1);
-  }
+  // if (!params.has('utm_p1') || params.get('utm_p1') === 'null' || params.get('utm_p1').trim() === '') {
+  //   addParamToUrl('utm_p1', utm_p1);
+  // }
   if (!params.has('utm_p2') || params.get('utm_p2') === 'null' || params.get('utm_p2').trim() === '') {
     addParamToUrl('utm_p2', utm_p2);
   }
